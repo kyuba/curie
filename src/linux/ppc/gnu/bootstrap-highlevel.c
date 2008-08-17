b6fedeb8cfed3b82ac39bee71bf9091ea5569055
Magnus Deininger <dma05@web.de>
2008-08-17 23:04:25 +0000

    the bootstrap code seems to be working

---
+++ src/linux/ppc/gnu/bootstrap-highlevel.c
@@ -0,0 +1,66 @@
+/*
+ *  linux-ppc-gnu/bootstrap-highlevel.c
+ *  atomic-libc
+ *
+ *  Created by Magnus Deininger on 17/08/2008.
+ *  Copyright 2008 Magnus Deininger. All rights reserved.
+ *
+ */
+
+/*
+ * Copyright (c) 2008, Magnus Deininger All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer. *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution. *
+ * Neither the name of the project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <atomic/main.h>
+#include <atomic/memory.h>
+#include <atomic/immutable.h>
+
+char ** __do_startup (char **y, int argc, int envc) {
+    int i, j;
+
+    atomic_argv = get_mem ((argc + 1) * sizeof (char *));
+    atomic_environment = get_mem ((envc + 1) * sizeof (char *));
+
+    for (i = 1, j = 0; y[i] != (char *)0; i++) {
+        if (y[i][0] != (char)0) {
+            atomic_argv[j] = str_immutable_unaligned(y[i]);
+            j++;
+        }
+    }
+    atomic_argv[j] = (char *)0;
+
+    for (i++, j = 0; y[i] != (char *)0; i++) {
+        if (y[i][0] != (char)0) {
+            atomic_environment[j] = str_immutable_unaligned(y[i]);
+            j++;
+        }
+    }
+    atomic_environment[j] = (char *)0;
+
+    return y;
+}
