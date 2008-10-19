4f932cd6d5e53d532c28d474c5408063170508e8
Magnus Deininger <dma05@web.de>
2008-10-19 01:02:25 +0100

    document network.h

--- include/generic/curie/network.h
+++ include/generic/curie/network.h
@@ -52,31 +52,94 @@
 extern "C" {
 #endif
 
+/*! \brief Open Network Loop
+ *  \param[out] in  Input I/O structure for the loop.
+ *  \param[out] out Output I/O structure for the loop.
+ *
+ *  This opens a set of io structures, where if you write to the one returned
+ *  to the out parameter, it shows up on the in paramter. You know, like pipe()
+ *  or socketpair(). Usually socketpair is used, since pipe() is naughty and may
+ *  generate SIGPIPEs.
+ *
+ *  Both in and out may be (struct io *)0 if the loop couldn't be opened.
+ */
 void net_open_loop (/*@out@*/ /*@notnull@*/ struct io **in,
                     /*@out@*/ /*@notnull@*/ struct io **out);
 
+/*! \brief Connect to Socket
+ *  \param[in]  path The socket to connect to.
+ *  \param[out] in   I/O structure to read from.
+ *  \param[out] out  I/O structure to write to.
+ *
+ *  Connet to a Unix socket and return proper in/out I/O structures.
+ *
+ *  Both in and out may be (struct io *)0 if the loop couldn't be opened.
+ */
 void net_open_socket (/*@notnull@*/ const char *path,
                       /*@out@*/ /*@notnull@*/ struct io **in,
                       /*@out@*/ /*@notnull@*/ struct io **out);
 
+/*! \brief Accept Connections on Network Sockets
+ *
+ *  Call this function before using either multiplex_add_socket() or
+ *  multiplex_add_socket_listener_sx().
+ */
 void multiplex_network();
+
+/*! \brief Listen on a Unix Socket
+ *  \param[in] path       The socket to connect to.
+ *  \param[in] on_connect Called when a new connection is established.
+ *  \param[in] aux        Passed to the callback function.
+ *
+ *  This multiplexer function will create a new Unix socket at the given path
+ *  and call the on_connect() function when a new connection is establish. The
+ *  on_connect() gets an input I/O structure as the first argument, and an
+ *  output structure as its second argument.
+ *
+ *  \note The in/out I/O structures aren't added to the I/O multiplexer
+ *        automatically, you'll have to do that yourself.
+ */
 void multiplex_add_socket
         (/*@notnull@*/ const char *path,
          /*@notnull@*/ void (*on_connect)(struct io *, struct io *, void *),
-         /*@null@*/ void *data);
+         /*@null@*/ void *aux);
 
+/*! \brief Listen on a Unix Socket with S-Expression I/O
+ *  \param[in] path       The socket to connect to.
+ *  \param[in] on_connect Called when a new connection is established.
+ *  \param[in] aux        Passed to the callback function.
+ *
+ *  Analoguous to multiplex_add_socket(), except that the I/O structures are
+ *  fed to the S-Expression code right away.
+ *
+ *  \note The sexpr_io structure isn't added to the s-expression multiplexer
+ *        automatically, you'll have to do that yourself.
+ */
 void multiplex_add_socket_listener_sx
         (/*@notnull@*/ const char *path,
          /*@notnull@*/ void (*on_connect)(struct sexpr_io *, void *),
-         /*@null@*/ void *data);
+         /*@null@*/ void *aux);
 
+/*! \brief Connect to Socket with S-Expression I/O
+ *  \param[in] path The socket to connect to.
+ *  \return New sexpr_io structure, or (struct sexpr_io *)0 for errors.
+ *
+ *  Analoguous to net_open_socket(), just for S-Expression I/O.
+ */
 /*@null@*/ /*@only@*/ struct sexpr_io *sx_open_socket
         (/*@notnull@*/ const char *path);
 
+/*! \brief Open Socket and register Callback for S-Expression I/O
+ *  \param[in] path    The socket to connect to.
+ *  \param[in] on_read Callback function when new data comes in.
+ *  \param[in] aux     Arbitrary data, passed to the callback function.
+ *
+ *  Same as multiplex_add_sexpr, but it opens the given socket and uses that.
+ */
 void multiplex_add_socket_sx
         (/*@notnull@*/ const char *path,
          /*@null@*/ void (*on_read)(/*@shared@*/ struct sexpr *, struct sexpr_io *, void *),
-         /*@null@*/ void *d);
+         /*@null@*/ void *aux);
 
 #ifdef __cplusplus
 }
