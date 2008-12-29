ded9a8fc6ee7250bb0eda20832361a477c54a410
Magnus Deininger <dma05@web.de>
2008-12-29 05:57:08 +0000

    docs for directory.h

--- include/generic/curie/directory.h
+++ include/generic/curie/directory.h
@@ -51,8 +51,54 @@ extern "C" {
 
 #include <curie/regex.h>
 
+/*! \brief Read Directory, filtered with a Regular Expression
+ *  \param[in] base The base directory to search.
+ *  \param[in] rx   Pattern to match against the directory entries.
+ *  \return A list of files in the directory.
+ *
+ *  Search the given directory and create a list of all files in the directory
+ *  that match the given regular expression. The list of files doesn't contain
+ *  the base path, just the filenames.
+ *
+ *  Example: read_directory_rx("/", rx_compile(".*"));
+ *  Would return something like this list:
+ *  ("bin" "lib" "dev" "sbin" "usr" "opt" "var" "etc" "root" "home")
+ */
 sexpr read_directory_rx (const char *base, struct graph *rx);
+
+/*! \brief Read Directory, filtered with an S-expression
+ *  \param[in] rx   The Pattern to use.
+ *  \return A list of files matching the pattern.
+ *
+ *  Simple wrapper around read_directory_rx() and read_directory(). If rx is a
+ *  string, the string is passed to read_directory. If it's a cons, it's assumed
+ *  to be an NFA graph in s-expression form, the graph is converted and passed
+ *  to read_directory() with "." as the base path.
+ */
 sexpr read_directory_sx (sexpr rx);
+
+/*! \brief Read Directory, filtered with a C-style string
+ *  \param[in] p    The Pattern to use.
+ *  \return A list of files matching the pattern.
+ *
+ *  The string is split by '/' and each of the tokens is used as a regular
+ *  expression to ultimately specify a number of files to find. It's annoying to
+ *  describe verbally, so an example is probably best:
+ *
+ *  read_directory("/.+/lib");
+ *  should yield: ("/././lib" "/./usr/lib" "/./var/lib")
+ *
+ *  and
+ *
+ *  read_directory(".+/lib");
+ *  could yield (if you're in the root directory):
+ *  ("././lib" "./usr/lib" "./var/lib")
+ *
+ *  Note that a leading "/" always results in a "/." prefix of the resulting
+ *  list and any other character results in a simple "." prefix. This is
+ *  technically overly verbose, but it shouldn't really matter. Also, the list
+ *  of files that is returned are full paths, relative to the current directory.
+ */
 sexpr read_directory    (const char *p);
 
 #ifdef __cplusplus
