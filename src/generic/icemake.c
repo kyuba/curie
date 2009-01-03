5208788a6e50823db6877d7ce186f8208d056438
Magnus Deininger <dma05@web.de>
2009-01-03 18:01:40 +0000

    back to multiplex_all_processes for this bugger

--- src/generic/icemake.c
+++ src/generic/icemake.c
@@ -2823,8 +2823,7 @@ int main (int argc, char **argv, char **environ)
     stdio                   = sx_open_stdio();
 
     multiplex_io();
-/*    multiplex_all_processes();*/
-    multiplex_process();
+    multiplex_all_processes();
     multiplex_sexpr();
     multiplex_signal();
 
