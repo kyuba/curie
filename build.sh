9676ec7223c8be177252fa300c6e92805f815d76
Magnus Deininger <dma05@web.de>
2009-08-09 23:56:34 +0100

    icemake -> ice in build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 
 if ! [ -f build/bin/ice ] || ! [ -f build/bin/icemake ]; then
     if ./build-icemake.sh -foid build/; then
-        PATH="./build/bin/:${PATH}" exec icemake $@;
+        PATH="./build/bin/:${PATH}" exec ice $@;
     fi
 else
-    PATH="./build/bin/:${PATH}" exec icemake $@;
+    PATH="./build/bin/:${PATH}" exec ice $@;
 fi
