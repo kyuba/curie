2c4d9c9be3b0ad3af5660753afd1ec105f13b0d9
Magnus Deininger <dma05@web.de>
2009-10-09 20:28:57 +0100

    some minor opts

--- src/sexpr.c
+++ src/sexpr.c
@@ -44,15 +44,16 @@ sexpr cons(sexpr sx_car, sexpr sx_cdr)
             MEMORY_POOL_INITIALISER(sizeof (struct sexpr_cons));
     struct sexpr_cons *rv;
     struct tree_node *n;
-    struct tree *t2;
+    struct tree *t2 = (struct tree *)0;
 
-    if ((n = tree_get_node (&sx_cons_tree, (int_pointer)sx_car)) &&
-         ((t2 = (struct tree *)node_get_value (n)) != (struct tree *)0) &&
-        (n = tree_get_node (t2, (int_pointer)sx_cdr)) &&
-         ((rv = (struct sexpr_cons *)node_get_value (n))
-             != (struct sexpr_cons *)0))
+    if ((n = tree_get_node (&sx_cons_tree, (int_pointer)sx_car)))
     {
-        return (sexpr)rv;
+        t2 = (struct tree *)node_get_value (n);
+
+        if ((n = tree_get_node (t2, (int_pointer)sx_cdr)))
+        {
+            return (sexpr)node_get_value (n);
+        }
     }
 
     rv = get_pool_mem (&pool);
@@ -66,8 +67,7 @@ sexpr cons(sexpr sx_car, sexpr sx_cdr)
     rv->car  = sx_car;
     rv->cdr  = sx_cdr;
 
-    if (!(n = tree_get_node (&sx_cons_tree, (int_pointer)sx_car)) ||
-        !((t2 = (struct tree *)node_get_value (n)) != (struct tree *)0))
+    if (t2 == (struct tree *)0)
     {
         t2 = tree_create ();
 
@@ -89,12 +89,11 @@ static sexpr make_string_or_symbol (const char *string, char symbol)
     int_32 hash = str_hash (string, &len);
     struct tree_node *n;
 
-    if ((n = tree_get_node ((symbol == (char)1) ? &sx_symbol_tree : &sx_string_tree, (int_pointer)hash))) {
-        if ((s = (struct sexpr_string_or_symbol *)node_get_value (n))
-             != (struct sexpr_string_or_symbol *)0)
-        {
-            return (sexpr)s;
-        }
+    if ((n = tree_get_node ((symbol == (char)1) ? &sx_symbol_tree
+                                                : &sx_string_tree,
+                            (int_pointer)hash)))
+    {
+        return (sexpr)node_get_value (n);
     }
 
     if ((s = aalloc (sizeof (struct sexpr_string_or_symbol) + len + 1))
@@ -103,9 +102,12 @@ static sexpr make_string_or_symbol (const char *string, char symbol)
         return sx_nonexistent;
     }
 
-    tree_add_node_value ((symbol == (char)1) ? &sx_symbol_tree : &sx_string_tree, (int_pointer)hash, s);
+    tree_add_node_value ((symbol == (char)1) ? &sx_symbol_tree
+                                             : &sx_string_tree,
+                         (int_pointer)hash, s);
 
-    for (i = 0; string[i] != (char)0; i++) {
+    for (i = 0; i < len; i++)
+    {
         s->character_data[i] = string[i];
     }
     s->character_data[i] = (char)0;
@@ -139,32 +141,29 @@ void sx_destroy(sexpr sxx)
         unsigned long length = 0;
         int_32 hash;
 
-        hash = str_hash (((struct sexpr_string_or_symbol *)sx)->character_data, &length);
+        hash = str_hash (((struct sexpr_string_or_symbol *)sx)->character_data,
+                         &length);
 
-        if (sx->type == sxt_string) {
-            tree_remove_node(&sx_string_tree, (int_pointer)hash);
-        } else {
-            tree_remove_node(&sx_symbol_tree, (int_pointer)hash);
-        }
+        tree_remove_node ((sx->type == sxt_string) ? &sx_string_tree
+                                                   : &sx_symbol_tree,
+                          (int_pointer)hash);
 
-        afree ((sizeof (struct sexpr_string_or_symbol) + length), sx);
+        afree ((sizeof (struct sexpr_string_or_symbol) + length + 1), sx);
         gc_base_items--;
     }
     else if (consp(sxx))
     {
         struct sexpr_cons *sx = (struct sexpr_cons *)sx_pointer(sxx);
         struct tree *t2;
-        struct tree_node *n;
+        struct tree_node *n = tree_get_node (&sx_cons_tree,
+                                             (int_pointer)(sx->car));
 
-        if ((n = tree_get_node (&sx_cons_tree, (int_pointer)(sx->car))) &&
-            ((t2 = (struct tree *)node_get_value (n)) != (struct tree *)0))
+        t2 = (struct tree *)node_get_value (n);
+        tree_remove_node (t2, (int_pointer)(sx->cdr));
+
+        if (t2->root == (struct tree_node *)0)
         {
-            tree_remove_node (t2, (int_pointer)(sx->cdr));
-
-            if (t2->root == (struct tree_node *)0)
-            {
-                tree_remove_node (&sx_cons_tree, (int_pointer)(sx->car));
-            }
+            tree_remove_node (&sx_cons_tree, (int_pointer)(sx->car));
         }
 
         free_pool_mem (sx);
@@ -175,8 +174,7 @@ void sx_destroy(sexpr sxx)
         int type = sx_type (sxx);
         struct sexpr_type_descriptor *d = sx_get_descriptor (type);
 
-        if ((d != (struct sexpr_type_descriptor *)0) &&
-             (d->destroy != (void *)0))
+        if ((d->destroy != (void *)0))
         {
             d->destroy (sxx);
         }
