564dbde2c6520e6deb622088b54cd04112f9fa7f
Magnus Deininger <dma05@web.de>
2009-11-12 22:00:19 +0000

    use the actual system path instead of the minimal default path for subprocesses... ah well

--- src/icemake.c
+++ src/icemake.c
@@ -1656,9 +1656,10 @@ static void spawn_item (sexpr sx, void (*f)(struct exec_context *, void *))
     }
     else
     {
-        char *tenv[3] = { (char *)sx_string (rsx),
-                          "PATH=/bin",
-                          (char *)0 };
+        char *tenv[3] =
+            { (char *)sx_string (rsx),
+              getenv ("PATH"),
+              (char *)0 };
 
         context = execute (EXEC_CALL_NO_IO | EXEC_CALL_PURGE, ex, tenv);
     }
