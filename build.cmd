82978c8035f2eebc752222b0496c4ccec8427b0b
Magnus Deininger <dma05@web.de>
2011-03-03 00:53:00 +0100

    was installing to the wrong directory

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@ REM Build System Wrapper
 REM Win32/cmd Variant
 
 IF NOT EXIST build\bin\icemake.exe (
-    CALL build-icemake.cmd -fLoid build/
+    CALL build-icemake.cmd -fLoid build\ 
 )
 
 IF ERRORLEVEL 0 (
