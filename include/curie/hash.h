241443145f33d44fcd879af80402f12a7ca9a20d
Magnus Achim Deininger <magnus@ef.gy>
2014-01-14 15:04:00 +0000

    updated hash and internal constant header

--- include/curie/hash.h
+++ include/curie/hash.h
@@ -1,22 +1,26 @@
-/*
- * This file is part of the becquerel.org Curie project.
- * See the appropriate repository at http://git.becquerel.org/ for exact file
- * modification records.
-*/
-
-/*
- * Copyright (c) 2008-2014, Kyuba Project Members
+/**\file
+ * \brief Hash Functions
  *
+ * This file will contain a number of hash functions. Hash functions are rather
+ * useful in general, and needed for a variety of problems, including some
+ * of the basic functionality in curie.
+ *
+ * The hash functions in this file (should) be in the public domain, as far as
+ * I know, anyway.
+ *
+ * \copyright
+ * Copyright (c) 2008-2014, Kyuba Project Members
+ * \copyright
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * \copyright
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * \copyright
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,17 +28,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
-*/
-
-/*! \file
- *  \brief Hash Functions
  *
- *  This file will contain a number of hash functions. Hash functions are rather
- *  useful in general, and needed for a variety of problems, including some
- *  of the basic functionality in curie.
- *
- *  The hash functions in this file (should) be in the public domain, as far as
- *  I know, anyway.
+ * \see Projct Documentation: http://ef.gy/documentation/curie
+ * \see Project Source Code: http://git.becquerel.org/kyuba/curie.git
  */
 
 #ifndef LIBCURIE_HASH_H
@@ -46,45 +42,54 @@ extern "C" {
 
 #include <curie/int.h>
 
-/*! \brief MurmurHash2, 32-Bit
- *  \param[in] key  The data to hash.
- *  \param[in] len  Length (in bytes) of key.
- *  \param[in] seed Start value for the hash.
+/**\brief MurmurHash2, 32-Bit
  *
- *  This is the MurmurHash2, written by Austin Appleby. For more details, see
- *  http://murmurhash.googlepages.com/ -- this is the website where the hash
- *  is available in its original form.
+ * \param[in] key  The data to hash.
+ * \param[in] len  Length (in bytes) of key.
+ * \param[in] seed Start value for the hash.
  *
- *  This variant in particular is the regular 32-bit version of the hash.
+ * This is the MurmurHash2, written by Austin Appleby. For more details, see
+ * http://murmurhash.googlepages.com/ -- this is the website where the hash
+ * is available in its original form.
+ *
+ * This variant in particular is the regular 32-bit version of the hash.
+ *
+ * \returns The hash over the provided input, as described.
  */
 int_32      hash_murmur2_32 ( const void * key, int len, unsigned int seed );
 
-/*! \brief MurmurHash2, 64-Bit
- *  \param[in] key  The data to hash.
- *  \param[in] len  Length (in bytes) of key.
- *  \param[in] seed Start value for the hash.
+/**\brief MurmurHash2, 64-Bit
  *
- *  This is the MurmurHash2, written by Austin Appleby. For more details, see
- *  http://murmurhash.googlepages.com/ -- this is the website where the hash
- *  is available in its original form.
+ * \param[in] key  The data to hash.
+ * \param[in] len  Length (in bytes) of key.
+ * \param[in] seed Start value for the hash.
  *
- *  This variant in particular is either the native 64-bit version, or the
- *  version for 32-bit architectures, depending on the current architecture.
- *  Thus it's perfectly fine to use on 32-bit platforms.
+ * This is the MurmurHash2, written by Austin Appleby. For more details, see
+ * http://murmurhash.googlepages.com/ -- this is the website where the hash
+ * is available in its original form.
+ *
+ * This variant in particular is either the native 64-bit version, or the
+ * version for 32-bit architectures, depending on the current architecture.
+ * Thus it's perfectly fine to use on 32-bit platforms.
+ *
+ * \returns The hash over the provided input, as described.
  */
 int_64      hash_murmur2_64 ( const void * key, int len, unsigned int seed );
 
-/*! \brief MurmurHash2, Pointer-Sized
- *  \param[in] key  The data to hash.
- *  \param[in] len  Length (in bytes) of key.
- *  \param[in] seed Start value for the hash.
+/**\brief MurmurHash2, Pointer-Sized
  *
- *  This is the MurmurHash2, written by Austin Appleby. For more details, see
- *  http://murmurhash.googlepages.com/ -- this is the website where the hash
- *  is available in its original form.
+ * \param[in] key  The data to hash.
+ * \param[in] len  Length (in bytes) of key.
+ * \param[in] seed Start value for the hash.
  *
- *  This variant is either the 32-bit or the 64-bit version of the hash,
- *  depending on the pointer size of the target architecture.
+ * This is the MurmurHash2, written by Austin Appleby. For more details, see
+ * http://murmurhash.googlepages.com/ -- this is the website where the hash
+ * is available in its original form.
+ *
+ * This variant is either the 32-bit or the 64-bit version of the hash,
+ * depending on the pointer size of the target architecture.
+ *
+ * \returns The hash over the provided input, as described.
  */
 int_pointer hash_murmur2_pt ( const void * key, int len, unsigned int seed );
 
--- include/curie/internal-constants.h
+++ include/curie/internal-constants.h
@@ -1,22 +1,22 @@
-/*
- * This file is part of the becquerel.org Curie project.
- * See the appropriate repository at http://git.becquerel.org/ for exact file
- * modification records.
-*/
-
-/*
- * Copyright (c) 2008-2014, Kyuba Project Members
+/**\file
+ * \brief Constants (Internal)
  *
+ * These constants are likely to require tuning on different OSs and
+ * architectures, so they get their own header.
+ *
+ * \copyright
+ * Copyright (c) 2008-2014, Kyuba Project Members
+ * \copyright
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * \copyright
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * \copyright
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,13 +24,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
-*/
-
-/*! \file
- *  \brief Constants (Internal)
  *
- *  These constants are likely to require tuning on different OSs and
- *  architectures, so they get their own header.
+ * \see Projct Documentation: http://ef.gy/documentation/curie
+ * \see Project Source Code: http://git.becquerel.org/kyuba/curie.git
  */
 
 #ifndef LIBCURIE_INTERNAL_CONSTANTS_H
@@ -42,8 +38,23 @@
 extern "C" {
 #endif
 
+/**\brief sys_stat() buffer size
+ *
+ * Size of the buffer used to gather data via the sys_stat() call.
+ */
 #define LIBCURIE_STAT_BUFFER_SIZE 256
+
+/**\brief Directory entity buffer size
+ *
+ * Size of a single directory entity. Used when reading directory entities in
+ * a loop.
+ */
 #define LIBCURIE_DIRENT_BUFFER_SIZE 0x1000
+
+/**\brief Number of static pools
+ *
+ * The number of static memory pools as kept by the memory allocator.
+ */
 #define POOLCOUNT (LIBCURIE_PAGE_SIZE / ENTITY_ALIGNMENT)
 
 #ifdef __cplusplus
