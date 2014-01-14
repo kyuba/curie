b96822607a86bef357aa992015ea0f83f188de74
Magnus Achim Deininger <magnus@ef.gy>
2014-01-14 13:49:52 +0000

    updated Curie's math (rational number) header

--- include/curie/math.h
+++ include/curie/math.h
@@ -1,22 +1,23 @@
-/*
- * This file is part of the becquerel.org Curie project.
- * See the appropriate repository at http://git.becquerel.org/ for exact file
- * modification records.
-*/
-
-/*
- * Copyright (c) 2008-2014, Kyuba Project Members
+/**\file
+ * \brief Rational numbers
  *
+ * This file contains some code to work with rational numbers, which come in
+ * rather handy when you don't feel like relying on floating point arithmetic
+ * and its various quirks.
+ *
+ * \copyright
+ * Copyright (c) 2008-2014, Kyuba Project Members
+ * \copyright
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * \copyright
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * \copyright
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,20 +25,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
-*/
-
-/*! \file
- *  \brief Specific-length integers
  *
- *  This file is rather platform/architecture specific, so this header file
- *  may need porting, depending on the target architecture. for this very
- *  purpose, the build system is capable of feeding the compiler with
- *  appropriate path information to allow overrides in the include/ directory.
- *  this means, instead of mucking with wickedass macromagic, just add an
- *  appropriate directory under include/ and copy this file there.
- *
- *  This specific file is the generic version which so far seems to work across
- *  all the tested targets.
+ * \see Project Documentation: http://ef.gy/documentation/curie
+ * \see Project Source Code: http://git.becquerel.org/kyuba/curie.git
  */
 
 #ifndef LIBCURIE_MATH_H
@@ -49,39 +39,42 @@ extern "C" {
 
 #include <curie/int.h>
 
-/*! \brief Rational Number
+/**\brief Rational Number
  *
- *  Basic type to represent rational numbers.
+ * Basic type to represent rational numbers.
  *
- *  Note that the types were chosen to maximise the range of the numbers, at a
- *  slight disadvantage in resolution for the sub-integer portions, thus the
- *  sign is only used in the denominator, even though it would appear to be more
- *  natural to stick it in the numerator.
+ * Note that the types were chosen to maximise the range of the numbers, at a
+ * slight disadvantage in resolution for the sub-integer portions, thus the
+ * sign is only used in the denominator, even though it would appear to be more
+ * natural to stick it in the numerator.
  */
 struct rational
 {
-    /*! \brief Nominator
+    /**\brief Nominator
      *
-     *  Nominator of the rational number, i.e. the left/upper part of the
-     *  fraction.
+     * Nominator of the rational number, i.e. the left/upper part of the
+     * fraction.
      */
     int_pointer   numerator;
 
-    /*! \brief Denominator
+    /**\brief Denominator
      *
-     *  Denominator of the rational number, i.e. the lower/right part of the
-     *  fraction.
+     * Denominator of the rational number, i.e. the lower/right part of the
+     * fraction.
      */
     int_pointer_s denominator;
 };
 
-/*! \brief Calculate the Greatest Common Denominator
- *  \param[in] a Integer >= 1.
- *  \param[in] b Integer >= 1.
+/**\brief Calculate the Greatest Common Denominator
  *
- *  Calculate the greatest common denominator of a and b. We're using the
- *  euclidian algorithm for this, possibly also special variants depending on
- *  the architecture.
+ * \param[in] a Integer >= 1.
+ * \param[in] b Integer >= 1.
+ *
+ * Calculate the greatest common denominator of a and b. We're using the
+ * euclidian algorithm for this, possibly also special variants depending on
+ * the architecture.
+ *
+ * \returns The greatest common denominator of 'a' and 'b'.
  */
 int_64 gcd (int_64 a, int_64 b);
 
