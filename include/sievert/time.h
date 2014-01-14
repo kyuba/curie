830f9fae0c73b1630aa407691544973591b7903f
Magnus Achim Deininger <magnus@ef.gy>
2014-01-14 13:12:30 +0000

    update sievert time header and remove julian and gregorian date functions (which have never been implemented)

--- include/sievert/time.h
+++ include/sievert/time.h
@@ -1,22 +1,23 @@
-/*
- * This file is part of the becquerel.org Curie project.
- * See the appropriate repository at http://git.becquerel.org/ for exact file
- * modification records.
-*/
-
-/*
- * Copyright (c) 2008-2014, Kyuba Project Members
+/**\file
+ * \brief Date and Time Conversion Functions
  *
+ * Since there's at least a dozen different calendar and time systems in use
+ * these days, the functions in this header allow you to convert date and time
+ * between different formats.
+ *
+ * \copyright
+ * Copyright (c) 2008-2014, Kyuba Project Members
+ * \copyright
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * \copyright
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * \copyright
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,14 +25,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
-*/
-
-/*! \file
- *  \brief Date and Time Conversion Functions
  *
- *  Since there's at least a dozen different calendar and time systems in use
- *  these days, the functions in this header allow you to convert date and time
- *  between different formats.
+ * \see Project Documentation: http://ef.gy/documentation/curie
+ * \see Project Source Code: http://git.becquerel.org/kyuba/curie.git
  */
 
 #ifndef LIBSIEVERT_TIME_H
@@ -43,37 +39,10 @@ extern "C" {
 
 #include <curie/time.h>
 
-#define LEAP_YEAR (1 << 0)
-
-struct gregorian_date
-{
-    long int      year;
-    unsigned char month;
-    unsigned char day;
-    unsigned char modifiers;
-};
-
-struct julian_date
-{
-    long int      year;
-    unsigned char month;
-    unsigned char day;
-    unsigned char modifiers;
-};
-
 struct datetime dt_from_unix
     (unsigned long long timestamp);
-struct datetime dt_from_julian_day
-    (long double julian_day);
-struct datetime dt_from_julian_date
-    (struct julian_date julian_date);
-struct datetime dt_from_gregorian_date
-    (struct gregorian_date gregorian_date);
 
 unsigned long long    dt_to_unix           (struct datetime date);
-long double           dt_to_julian_day     (struct datetime date);
-struct julian_date    dt_to_julian_date    (struct datetime date);
-struct gregorian_date dt_to_gregorian_date (struct datetime date);
 
 #ifdef __cplusplus
 }
